2dd4671d90f9d7bb8309b379f459f3212ee81d18
Lexzino <alexudeze001@gmail.com>
2024-08-27 22:48:07 +0100

    task done

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,12 @@
-0-redis_client.js
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
